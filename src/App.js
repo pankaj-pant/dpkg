1038e93e9d2789babc1dc592796d65c83d4145ce
pankaj-pant <pankajpant89@gmail.com>
2020-08-11 21:33:56 +0300

    file selector

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const [file, setFile] = useState()
+  const loadFile = () => {
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Package Analyser</h2>
+      <input type="file" onChange={(event) => setFile(event.target.files[0])}></input>
+     
     </div>
   );
 }
